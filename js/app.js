54a3f8c92ce38c92c30a2352d2640fa2b7a73fb7
David Mandel <MandelDL@gmail.com>
2015-03-07 14:56:15 -0500

    create first route for DayController and views/day.html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 var app = angular.module('CalendarApp', [ngRoute]);
+
+// Routes
+
+app.config(function($routeProvider) {
+	$routeProvider
+	.when('/', {
+		controller: 'DayController',
+		templateUrl: 'views/day.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+});
